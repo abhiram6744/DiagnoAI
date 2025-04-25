a7690461b3daaddcb2013e37ef2c669fb3654aac
Abhiram <abhiramsai6744@gmail.com>
2025-04-25 21:06:03 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'ABHIRAM SAI',
+    author_email= 'abhiramsai6744@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
